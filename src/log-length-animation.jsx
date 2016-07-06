ba82ec6f2d4fab41f1e0bee66fe14fa5660d66d6
Buck Shlegeris <bshlegeris@gmail.com>
2016-07-06 00:13:01 -0700

    bundle things

--- src/log-length-animation.jsx
+++ src/log-length-animation.jsx
@@ -1,15 +1,17 @@
-var React = require("react");
-var ReactDom = require("react-dom");
+const React = require("react");
+const ReactDom = require("react-dom");
+
+const css = require("./log-length-animation.css");
 
 const LogLengthAnimation = React.createClass({
   getInitialState () {
     return {
-      number: 0,
-      rate: this.props.rate
+      number: 0
     }
   },
   componentDidMount () {
     this.restart();
+    this.rate = 0.3;
   },
   restart () {
     this.setState({ number: 1 });
@@ -23,8 +25,8 @@ const LogLengthAnimation = React.createClass({
       this.setState({
         number: Math.min(
           this.state.number +
-          10 * this.state.rate +
-          Math.round(Math.random() * this.state.rate), 999999)
+          10 * this.rate +
+          Math.round(Math.random() * this.rate), 999999)
       });
       setTimeout(() => {
         that.updateTime();
@@ -33,9 +35,6 @@ const LogLengthAnimation = React.createClass({
       this.setState({ number: 999999 });
       setTimeout(this.restart, 1000);
     }
-  },
-  changeRate (e) {
-    this.setState({rate: parseFloat(e.target.value)});
   },
   render () {
     const startString = this.state.number + "";
@@ -56,17 +55,8 @@ const LogLengthAnimation = React.createClass({
       <div>
         <button className="btn btn-default" onClick={this.restart}>Restart</button>
       </div>
-      <div>
-        <label>Meta control: Rate
-          <input type="number" value={this.state.rate} onChange={this.changeRate} />
-        </label>
-      </div>
     </div>;
   }
 });
 
-ReactDom.render(<LogLengthAnimation rate={13} />,
-      document.getElementById("log-length-animation"));
-
-
-
+exports.LogLengthAnimation = LogLengthAnimation;
--- src/log-length-demo.jsx
+++ src/log-length-demo.jsx
@@ -1,5 +1,7 @@
-var React = require("react");
-var ReactDom = require("react-dom");
+const React = require("react");
+const ReactDom = require("react-dom");
+
+const css = require("./log-length-demo.css");
 
 const LogLengthDemo = React.createClass({
   getInitialState () {
@@ -33,6 +35,56 @@ const LogLengthDemo = React.createClass({
   },
   render () {
     return <div>
+
+      <h2>Version 4</h2>
+
+      <p> Suggested compromise version: 3 lines, first line is like version 1,
+       second line is like "7 (in base 10) is 111 (in base 2)"
+       , third line is an underbrace under 111 saying "3 digits"
+       . Second line is left-aligned, spacing between lines is
+       similar to version 1.</p>
+
+      <div className="log-length-demo" style={{height: "200px"}}>
+        <div>
+          log
+          base
+          <input
+            className="big-input"
+            value={this.state.base}
+            onChange={this.changeBase}
+            min={2}
+            max={10}
+            type="number" />
+          of
+
+            <input
+            className="big-input"
+            value={this.state.number}
+            onChange={this.changeNumber}
+            min={1}
+            max={1000000}
+            type="number" />
+           {"≈ "}
+          {(Math.log(this.state.number) / Math.log(this.state.base)).toFixed(5)}
+        </div>
+
+        <div style={{marginTop: "10px"}}>
+          {this.state.number} (in base 10) is&nbsp;
+          <div style={{display: "inline-block", position: "relative"}}>
+            {this.numberString()}
+            <div style={{position: "absolute", display: "block", margin: "0 auto"}}>
+              <img className="brace" src="https://upload.wikimedia.org/wikipedia/commons/8/88/ThinBraceDown.svg" />
+              <div>
+                {this.numberString().length} digits
+              </div>
+            </div>
+          </div>
+          &nbsp;(in base {this.state.base})
+        </div>
+      </div>
+
+      <hr/>
+
       <h2>Version 1</h2>
       <div className="log-length-demo" style={{height: "200px"}}>
         log
@@ -130,7 +182,7 @@ const LogLengthDemo = React.createClass({
         </div>
         <div>
           {this.state.number}<sub>10</sub> =&nbsp;
-          <div style={{position: "relative", display: "inline-block", textAlign: "center"}}>
+          <div style={{position: "relative", display: "inline-block", textAlign: "center", marginBottom: "40px"}}>
             {this.numberString()}
             <div style={{
               position: "absolute",
@@ -147,12 +199,17 @@ const LogLengthDemo = React.createClass({
           <sub>{this.state.base}</sub>&nbsp;
         </div>
       </div>
+
+      <hr/>
+
     </div>;
   }
 })
 
-ReactDom.render(<LogLengthDemo />,
-      document.getElementById("log-length-demo"));
+// ReactDom.render(<LogLengthDemo />,
+//       document.getElementById("log-length-demo"));
 
 
 
+
+exports.LogLengthDemo = LogLengthDemo;
